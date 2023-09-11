3e54e7284a8de9e50139fcadee0177bfc7b0036d
Asherah Connor <ashe@kivikakk.ee>
2023-09-11 16:23:39 +1000

    run_job: additional context

--- src/main.rs
+++ src/main.rs
@@ -22,32 +22,53 @@ async fn main() -> Result<()> {
 }
 
 async fn run_job(name: &str, job: &Job) -> Result<()> {
-    println!("[{}] comprobando src ...", name);
+    let mut src = job
+        .src
+        .connect_reader()
+        .await
+        .with_context(|| format!("connecting reader {}", name))?;
 
-    let mut src = job.src.connect_reader().await?;
-
-    src.inbox().await?;
+    src.inbox()
+        .await
+        .with_context(|| format!("inboxing {}", name))?;
 
     loop {
         let mut dest: Option<Box<dyn endpoint::EndpointWriter>> = None;
 
-        for mail in src.read().await? {
+        for mail in src
+            .read()
+            .await
+            .with_context(|| format!("reading {}", name))?
+        {
             if mail.flagged {
-                println!("ya copiado, saltando ...");
+                println!("[{}] ya copiado, saltando ...", name);
             } else {
                 let d = match dest {
                     Some(ref mut d) => d,
-                    None => dest.insert(job.dest.connect_writer().await?),
+                    None => dest.insert(
+                        job.dest
+                            .connect_writer()
+                            .await
+                            .with_context(|| format!("connecting writer {}", name))?,
+                    ),
                 };
-                d.append(&mail).await?;
-                src.flag(mail.uid).await?;
+                d.append(&mail)
+                    .await
+                    .with_context(|| format!("appending {}", name))?;
+                src.flag(mail.uid)
+                    .await
+                    .with_context(|| format!("flagging {}", name))?;
             }
         }
 
         if let Some(mut d) = dest {
-            d.disconnect().await?;
+            d.disconnect()
+                .await
+                .with_context(|| format!("disconnecting {}", name))?;
         }
 
-        src.idle().await?;
+        src.idle()
+            .await
+            .with_context(|| format!("idling {}", name))?;
     }
 }
